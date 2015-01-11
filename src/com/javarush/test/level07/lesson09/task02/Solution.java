ab5f847d2c4a53c7ac5f9f8973a8d7d0de1179b6
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-11 15:11:28 +0300

    level 7 lesson 9 task 2

---
+++ src/com/javarush/test/level07/lesson09/task02/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level07.lesson09.task02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/* 5 слов в обратном порядке
+Введи с клавиатуры 5 слов в список строк. Выведи их в обратном порядке.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++)
+        {
+            list.add(scanner.nextLine());
+        }
+        for (int i = list.size() - 1 ; i >= 0 ; i--)
+        {
+            System.out.println(list.get(i));
+        }
+    }
+}
