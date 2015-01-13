8efa86096c77ebb4a561a0648f8b7a97ac5e1da2
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-13 22:02:08 +0300

    level 8 lesson 11 home 4

---
+++ src/com/javarush/test/level08/lesson11/home04/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.test.level08.lesson11.home04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+/* Минимальное из N чисел
+1. Ввести с клавиатуры число N.
+2. Считать N целых чисел и заполнить ими список - метод getIntegerList.
+3. Найти минимальное число среди элементов списка - метод getMinimum.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        //find minimum here - найти минимум тут
+        int min = array.get(0);
+        for (Integer integer : array)
+        {
+            if (integer < min){
+                min = integer;
+            }
+        }
+        return min;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        //create and initialize a list here - создать и заполнить список тут
+        List<Integer> list = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++)
+        {
+            list.add(scanner.nextInt());
+        }
+        return list;
+    }
+}
