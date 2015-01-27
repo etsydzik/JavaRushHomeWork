f84a100bb536b20c6de0af68449dfbf57b90959a
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-27 17:05:21 +0300

    level 14 lesson 4 task 1

--- src/com/javarush/test/level14/lesson04/task01/Solution.java
+++ src/com/javarush/test/level14/lesson04/task01/Solution.java
@@ -20,16 +20,16 @@ public class Solution
     public static Building getSchool()
     {
         //измените null на объект класса Building или School
-        return null;
+        return new School();
     }
 
     public static Building getBuilding()
     {
         //измените null на объект класса Building или School
-        return null;
+        return new Building();
     }
 
-    static class School /*Add your code here*/
+    static class School extends Building
     {
         @Override
         public String toString()
