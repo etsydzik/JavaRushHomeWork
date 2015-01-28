f8437fac22e7b7f2dfa2f0d4725fb5792ad51751
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-28 16:37:11 +0300

    level 14 lesson 8 home 1

---
+++ src/com/javarush/test/level14/lesson08/home01/Bridge.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by tsyd on 28.01.2015.
+ */
+public interface Bridge
+{
+    int getCarsCount();
+}
---
+++ src/com/javarush/test/level14/lesson08/home01/SuspensionBridge.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by tsyd on 28.01.2015.
+ */
+public class SuspensionBridge implements Bridge
+{
+    @Override
+    public int getCarsCount()
+    {
+        return 10;
+    }
+}
