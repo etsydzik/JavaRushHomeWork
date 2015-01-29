ade75f1db885a05c08d5ea33aebd0c2ee0e5b5a5
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-29 20:00:05 +0300

    level 14 lesson 8 bonus 2

--- src/com/javarush/test/level14/lesson08/bonus02/Solution.java
+++ src/com/javarush/test/level14/lesson08/bonus02/Solution.java
@@ -6,9 +6,27 @@ package com.javarush.test.level14.lesson08.bonus02;
 Вывести в консоль наибольший общий делитель.
 */
 
+import java.util.Scanner;
+
 public class Solution
 {
     public static void main(String[] args) throws Exception
     {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        nod(a,b);
+    }
+
+    static void nod(int a, int b)
+    {
+        int x = Math.min(a, b);
+        int y = Math.max(a, b);
+        int result;
+        if (y % x == 0){
+            System.out.println(x);
+        }else{
+            nod(y % x, x);
+        }
     }
 }
