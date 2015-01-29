6639d5b7a57651f5bb2a915337b38a756ea6dcf6
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-01-29 14:36:57 +0300

    level 14 lesson 8 home 5

---
+++ src/com/javarush/test/level14/lesson08/home05/CompItem.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by tsyd on 29.01.2015.
+ */
+public interface CompItem
+{
+    String getName();
+}
---
+++ src/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by tsyd on 29.01.2015.
+ */
+public class Mouse implements CompItem
+{
+    @Override
+    public String getName()
+    {
+        return this.getClass().getSimpleName();
+    }
+}
