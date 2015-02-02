869b03bf32962faeef40b2f2f9fac5c69b56f039
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-02 13:23:48 +0300

    level 15 lesson 6 task 2

--- src/com/javarush/test/level15/lesson06/task02/Solution.java
+++ src/com/javarush/test/level15/lesson06/task02/Solution.java
@@ -15,14 +15,12 @@ public class Solution {
     }
 
     public static class NakedCat {
-        public NakedCat() {
-            super();
-        }
+
     }
 
     public static class NormalCat extends NakedCat {
         public NormalCat() {
-            super();
+
         }
 
         public NormalCat(String name) {
