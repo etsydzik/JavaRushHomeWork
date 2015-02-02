48397162463a98a59efcdfacd01a54dd4f64b0a2
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-02 16:57:16 +0300

    level 15 lesson 9 task 1

--- src/com/javarush/test/level15/lesson09/task01/Solution.java
+++ src/com/javarush/test/level15/lesson09/task01/Solution.java
@@ -7,10 +7,21 @@ import java.util.Map;
 В статическом блоке инициализировать labels 5 различными парами.
 */
 
-public class Solution {
+public class Solution
+{
     public static Map<Double, String> labels = new HashMap<Double, String>();
 
-    public static void main(String[] args) {
+    static
+    {
+        labels.put(2.0, "a");
+        labels.put(15.0d, "b");
+        labels.put(111.23, "c");
+        labels.put(0.1, "d");
+        labels.put(4975324.324659678, "e");
+    }
+
+    public static void main(String[] args)
+    {
         System.out.println(labels);
     }
 }
