8b06deb3176ccf937e3d1f231ead77268abbf269
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-03 18:06:35 +0300

    level 15 lesson 12 home 3

--- src/com/javarush/test/level15/lesson12/home03/Solution.java
+++ src/com/javarush/test/level15/lesson12/home03/Solution.java
@@ -15,18 +15,20 @@ public class Solution {
     public static void main(String[] args) {
         //Блок 2.
         //Вызов для Object (задание 2)
-        new Tree().info(new Integer("4"));
-        new Tree().info(new Short("4"));
-        new Tree().info(new BigDecimal("4"));
+        new Tree().info((Object)new Integer("4"));
+        new Tree().info((Object)new Short("4"));
+        new Tree().info((Object)new BigDecimal("4"));
 
         //Блок 3.
         //Вызов для Number (Задание 3)
+//        new Tree().info(new Number());
         new Tree().info(new Integer("4"));
         new Tree().info(new Short("4"));
         new Tree().info(new BigDecimal("4"));
 
         //Блок 4.
         //Вызов для String (Задание 4)
+
         new Tree().info(new String("4"));
         new Tree().info(new Integer("4").toString());
         new Tree().info(new Short("4").toString());
