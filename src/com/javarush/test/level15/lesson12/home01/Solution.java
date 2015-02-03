54ba395f6f8c92b85aee392dc8ca01be9f19c03e
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-03 16:42:48 +0300

    level 15 lesson 12 home 1

--- src/com/javarush/test/level15/lesson12/home01/Solution.java
+++ src/com/javarush/test/level15/lesson12/home01/Solution.java
@@ -14,46 +14,59 @@ import java.io.InputStreamReader;
 2.4. иначе, вызвать метод print для String.
 */
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
         //напиште тут ваш код
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(inputStreamReader);
         String s;
-        while (!(s = reader.readLine()).equals("exit")){
+        while (!(s = reader.readLine()).equals("exit"))
+        {
             try
             {
                 if (s.contains(".")){
                     double a = Double.parseDouble(s);
                     print(a);
-                }else if (0 < Short.parseShort(s) && Short.parseShort(s) < 128){
-                    short a = Short.parseShort(s);
-                    print(a);
-                }else if (128 <= Long.parseLong(s)){
-                    int a = Integer.parseInt(s);
-                    print(a);
-                } else {
-                    print(s);
+                }else {
+                    Integer integer = Integer.valueOf(s);
+                    if (integer < 0) {
+                        print(s);
+                    } else if (integer < 128) {
+                        print(integer.shortValue());
+                    } else {
+                        print(integer);
+                    }
                 }
-            } catch (NumberFormatException e){
-               print(s);
             }
+            catch (NumberFormatException e)
+            {
+                print(s);
+            }
+
+
+
         }
     }
 
-    public static void print(Double value) {
+    public static void print(Double value)
+    {
         System.out.println("Это тип Double, значение " + value);
     }
 
-    public static void print(String value) {
+    public static void print(String value)
+    {
         System.out.println("Это тип String, значение " + value);
     }
 
-    public static void print(short value) {
+    public static void print(short value)
+    {
         System.out.println("Это тип short, значение " + value);
     }
 
-    public static void print(Integer value) {
+    public static void print(Integer value)
+    {
         System.out.println("Это тип Integer, значение " + value);
     }
 }
