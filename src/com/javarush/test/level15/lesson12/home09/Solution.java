ffb94e144db10ed91d017694d9fd587a55d4fff0
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-04 15:54:50 +0300

    level 15 lesson 12 home 9

--- src/com/javarush/test/level15/lesson12/home09/Solution.java
+++ src/com/javarush/test/level15/lesson12/home09/Solution.java
@@ -22,16 +22,63 @@ obj name
 double 3.14
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
         //add your code here
+        Scanner scanner = new Scanner(System.in);
+        String url = scanner.nextLine();
+
+        int i = url.indexOf("?") + 1;
+        String stringAfterQuestionMark = url.substring(i);
+
+        String[] split = stringAfterQuestionMark.split("&");
+
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<String> obj = new ArrayList<>();
+
+        for (String s : split)
+        {
+            String[] key = s.split("=");
+            strings.add(key[0]);
+            if (key[0].equals("obj"))
+            {
+                obj.add(key[1]);
+            }
+        }
+
+        for (String s : strings)
+            System.out.print(s + " ");
+        if (obj.size()!= 0)
+        {
+            System.out.println();
+            for (String s : obj)
+            {
+                try
+                {
+                    Double doubleValue = Double.parseDouble(s);
+                    alert(doubleValue);
+                }
+                catch (Exception e)
+                {
+                    alert(s);
+                }
+            }
+        }
     }
 
-    public static void alert(double value) {
+
+    public static void alert(double value)
+    {
         System.out.println("double " + value);
     }
 
-    public static void alert(String value) {
+    public static void alert(String value)
+    {
         System.out.println("String " + value);
     }
 }
