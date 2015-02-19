3e9f499fa99221fb0c386498edf5161064be9fa4
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-19 13:31:50 +0300

    level 16 lesson 10 task 2

--- src/com/javarush/test/level16/lesson10/task02/Solution.java
+++ src/com/javarush/test/level16/lesson10/task02/Solution.java
@@ -11,21 +11,50 @@ package com.javarush.test.level16.lesson10.task02;
 PS: метод sleep выбрасывает InterruptedException.
 */
 
-public class Solution {
+public class Solution
+{
     public static volatile int countSeconds = 3;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException
+    {
         RacingClock clock = new RacingClock();
         //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
     }
 
-    public static class RacingClock extends Thread {
-        public RacingClock() {
+    public static class RacingClock extends Thread
+    {
+        public RacingClock()
+        {
             start();
         }
 
-        public void run() {
+        public void run()
+        {
             //add your code here - добавь код тут
+
+            try
+            {
+                while (true)
+                {
+                    if (countSeconds != 0){
+                        System.out.print(countSeconds + " ");
+                    }else{
+                        System.out.print("Марш!");
+                    }
+                    countSeconds--;
+                    Thread.sleep(1000);
+                    if (countSeconds == 0)
+                    {
+                        System.out.println("Марш!");
+                        return;
+                    }
+                }
+            }
+            catch (InterruptedException e) {
+                System.out.println("Прервано!");
+            }
         }
     }
 }
