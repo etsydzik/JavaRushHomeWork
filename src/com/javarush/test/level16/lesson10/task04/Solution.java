8ddaf981b28d5b1ff3fde99379e356399d712aa9
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-19 14:38:13 +0300

    level 16 lesson 10 task 4

--- src/com/javarush/test/level16/lesson10/task04/Solution.java
+++ src/com/javarush/test/level16/lesson10/task04/Solution.java
@@ -6,26 +6,37 @@ package com.javarush.test.level16.lesson10.task04;
 Нельзя использовать метод interrupt.
 */
 
-public class Solution {
-    public static void main(String[] args) throws InterruptedException {
+public class Solution
+{
+    public static void main(String[] args) throws InterruptedException
+    {
         Thread t = new Thread(new TestThread());
         t.start();
         Thread.sleep(3000);
         ourInterruptMethod();
     }
 
-    public static void ourInterruptMethod() {
-        //Add your code here - добавьте код тут
+    public static boolean cancel = false;
 
+    public static void ourInterruptMethod()
+    {
+        //Add your code here - добавьте код тут
+        cancel = true;
     }
 
-    public static class TestThread implements Runnable {
-        public void run() {
-            while(true) {
-                try {
+    public static class TestThread implements Runnable
+    {
+        public void run()
+        {
+            while (!cancel)
+            {
+                try
+                {
                     System.out.println("he-he");
                     Thread.sleep(500);
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
                 }
             }
         }
