4c5ed4460f5e747caa54dec9c7ac646299f54ac7
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-23 15:00:55 +0300

    level 16 lesson 13 home1

--- src/com/javarush/test/level16/lesson13/home01/Solution.java
+++ src/com/javarush/test/level16/lesson13/home01/Solution.java
@@ -23,10 +23,15 @@ public class Solution {
             }
         }
         public void printMsg() {
-            Thread t = null;//присвой переменной t текущую нить
+            Thread t = Thread.currentThread();//присвой переменной t текущую нить
             String name = t.getName();
             System.out.println("name=" + name);
-            //add sleep here - добавь sleep тут
+//            add sleep here - добавь sleep тут
+            try
+            {
+                Thread.sleep(1);
+
+            }catch (InterruptedException e){}
         }
     }
 }
