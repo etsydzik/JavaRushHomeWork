cbe6d0c3884f56d07015f2098f83c83fdd1b91f5
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-02-23 18:16:11 +0300

    level 16 lesson 13 home 6

--- src/com/javarush/test/level16/lesson13/home06/Solution.java
+++ src/com/javarush/test/level16/lesson13/home06/Solution.java
@@ -5,42 +5,81 @@ package com.javarush.test.level16.lesson13.home06;
 По образу и подобию CountDownRunnable создай нить CountUpRunnable, которая выводит значения в нормальном порядке - от 1 до number
 */
 
-public class Solution {
+public class Solution
+{
     public static int number = 5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         new CountDownRunnable("было");
         new CountUpRunnable("стало");
     }
 
     //Add your code below - добавьте код ниже
-    public static class CountUpRunnable {
-        public CountUpRunnable(String name) {
-        }
-    }
-
-    public static class CountDownRunnable implements Runnable {
-        private int countIndexDown = Solution.number;
+    public static class CountUpRunnable implements Runnable
+    {
+        private int countIndexDown = 1;
         private Thread t;
 
-        public CountDownRunnable(String name) {
+        public CountUpRunnable(String name)
+        {
             t = new Thread(this, name);
             t.start();
         }
 
-        public void run() {
-            try {
-                while (true) {
+        public void run()
+        {
+            try
+            {
+                while (true)
+                {
+                    System.out.println(toString());
+                    countIndexDown += 1;
+                    if (countIndexDown > number) return;
+                    Thread.sleep(500);
+                }
+            }
+            catch (InterruptedException e)
+            {
+            }
+        }
+
+        public String toString()
+        {
+            return t.getName() + ": " + countIndexDown;
+        }
+    }
+
+    public static class CountDownRunnable implements Runnable
+    {
+        private int countIndexDown = Solution.number;
+        private Thread t;
+
+        public CountDownRunnable(String name)
+        {
+            t = new Thread(this, name);
+            t.start();
+        }
+
+        public void run()
+        {
+            try
+            {
+                while (true)
+                {
                     System.out.println(toString());
                     countIndexDown -= 1;
                     if (countIndexDown == 0) return;
                     Thread.sleep(500);
                 }
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
             }
         }
 
-        public String toString() {
+        public String toString()
+        {
             return t.getName() + ": " + countIndexDown;
         }
     }
