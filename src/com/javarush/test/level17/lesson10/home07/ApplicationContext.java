d7ba12e08972f732c0782456ddd7273583bfcac3
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2015-05-19 16:31:19 +0300

    level 17 lesson 10 home 7

--- src/com/javarush/test/level17/lesson10/home07/ApplicationContext.java
+++ src/com/javarush/test/level17/lesson10/home07/ApplicationContext.java
@@ -8,20 +8,24 @@ ApplicationContext будет доступен множеству нитей.
 Сделать так, чтобы данные не терялись: подумай, какое ключевое слово необходимо поставить и где.
 */
 
-public abstract class ApplicationContext<GenericsBean extends Bean> {
+public abstract class ApplicationContext<GenericsBean extends Bean>
+{
     private Map<String, GenericsBean> container = new HashMap<String, GenericsBean>();
     //Map<Name, some class implemented Bean interface>
 
 
-    protected ApplicationContext() {
+    protected ApplicationContext()
+    {
         parseAllClassesAndInterfaces();
     }
 
-    public GenericsBean getByName(String name) {
-          return container.get(name);
+    public synchronized GenericsBean getByName(String name)
+    {
+        return container.get(name);
     }
 
-    public GenericsBean removeByName(String name) {
+    public synchronized GenericsBean removeByName(String name)
+    {
         return container.remove(name);
     }
 
